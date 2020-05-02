13653b220be87c5e31328db3cd68eb92f9993acd
jamc96 <jamc.mejia@gmail.com>
2020-05-01 23:20:15 -0600

    basic application set up

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import "reflect-metadata";
+import { createConnection, getConnectionOptions } from "typeorm";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { AuthenticationResolver } from "./resolvers/user/AuthenticationResolver";
+import { EmployeeResolver } from "./resolvers/employee/EmployeeResolver";
+import { VacationResolver } from "./resolvers/vacation/VacationResolver";
+
+(async () => {
+  const app = express();
+
+  const options = await getConnectionOptions(
+    process.env.NODE_ENV || "development"
+  );
+  await createConnection({ ...options, name: "default" });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [
+        AuthenticationResolver,
+        EmployeeResolver,
+        VacationResolver,
+      ],
+      validate: true
+    }),
+    context: ({ req, res }) => ({ req, res })
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false });
+  const port = process.env.PORT || 4000;
+  app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}/graphql`);
+  });
+})();
